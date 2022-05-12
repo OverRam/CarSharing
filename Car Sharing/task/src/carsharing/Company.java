f6679ce4a1d0b867957d9ce993083174d7c67616
OverRam <yugini666@gmail.com>
2022-05-12 23:13:16 +0200

    Implement Company class

---
+++ Car Sharing/task/src/carsharing/Company.java
@@ -0,0 +1,27 @@
+package carsharing;
+
+public class Company {
+    private String name;
+    private Integer id;
+
+    public Company(Integer id, String name) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
